d79993d4dca4093196b120fcc6e0519a3820a588
DouglasPenn23 <Douglaspennington2013@gmail.com>
2023-04-12 23:18:49 -0400

    Added images, fixed bugs

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+# Import Models you want to register on admin page
+from projects.models import Project
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the models with the admin classes
+admin.site.register(Project, ProjectAdmin)
